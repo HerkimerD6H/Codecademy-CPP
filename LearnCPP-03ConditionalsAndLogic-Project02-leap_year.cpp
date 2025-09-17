9cb193d172c89510ec41ba5af45d88180aaff4cb
Cat <cleverasapike@hotmail.com>
2025-09-17 19:07:40 -0400

    Create LearnCPP-03ConditionalsAndLogic-Project02-leap_year.cpp

---
+++ LearnCPP-03ConditionalsAndLogic-Project02-leap_year.cpp
@@ -0,0 +1,20 @@
+// Date File Added:   2025-09-17
+// Platform:          Codecademy
+// User Name:         @HerkimerD6H 
+// Profile Link:      https://www.codecademy.com/profiles/HerkimerD6H
+// Class:             Learn C++ (https://www.codecademy.com/enrolled/courses/learn-c-plus-plus)
+// Lesson:            03 - Conditionals and Variables
+// Project Name:      02 - Leap Year
+// Project File:      leap_year.cpp
+      //Instructions
+      // Optional: Let’s try a challenging problem that will put all your brain cells to the test.
+      // Write a leap_year.cpp program that:
+            // Takes a year as input.
+            // Checks to see if the year is a four-digit number.
+            // Displays whether or not the year falls on a leap year.
+      // There are 3 criteria that must be taken into account to identify a leap year:
+            // If the year can be evenly divided by 4 then it is a leap year, however…
+            // If that year can be evenly divided by 100, and it is not evenly divided by 400, then it is NOT a leap year.
+            // If that year is evenly divisible by 400, then it is a leap year.
+      // Take some time to research and brainstorm before starting to write the code!
+
