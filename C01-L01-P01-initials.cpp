00cbb8897807d6838f5f785cd062305ba9f550a7
Cat <cleverasapike@hotmail.com>
2025-09-17 18:20:09 -0400

    Create C01-L01-P01-initials.cpp
    Class: Learn C++ 
    Lesson: 01 - Hello World
    Project Name:  01 - Block Letters
    Project File on Codecademy: initials.cpp
    Instructions:  Write a C++ program called initials.cpp that displays the initials of your name in block letters

---
+++ C01-L01-P01-initials.cpp
@@ -0,0 +1,47 @@
+// Date File Added:   2025-09-17
+// Platform:          Codecademy
+// User Name:         @HerkimerD6H 
+// Profile Link:      https://www.codecademy.com/profiles/HerkimerD6H
+// Class:             Learn C++ (https://www.codecademy.com/enrolled/courses/learn-c-plus-plus)
+// Lesson:            01 - Hello World
+// Project Name:      01 - Block Letters
+// Project File:      initials.cpp
+// Instructions:      Write a C++ program called initials.cpp that displays the initials of your name in block letters as shown:
+        // Block Letters
+        // Tasks
+        // 8/8 complete  Mark the tasks as complete by checking them off
+        // What we are building in this project:
+        // 1. My initials are S and L, so my initials.cpp program should output: (initials). What are your initials? Take a look at the complete alphabet.
+        // Setting up:
+        // 2. First, write a comment with a fun fact about yourself!
+        // 3. And let’s create a skeleton for the program:
+              // #include <iostream>
+              // int main() {
+              // Your program lives here:
+              // }
+              // Output your initials:
+       // 4. Output your first initial. Press Save to save your program.
+       // 5. Compile and execute to make sure you got the result. To compile, type the following after the $ sign and press enter: g++ initials.cpp. To execute, type the following and press enter: ./a.out
+       // 6. Output your second initial. Press Save to save your program.
+       // 7. Compile and execute to make sure you got the result.
+       // 8. If you get stuck during the project or would like to see an experienced developer work through the project, watch the project walkthrough video.
+
+
+//******** My Project Begins Here:************//
+
+// My fun fact is that I have NVLD, which is a visual-spatial learning disability, and frankly, I am nervous about trying to get the spacing correct! 
+// I didn't know I had NVLD 20 years ago when I was trying to learn programming the first time! I am appreciative of the clearly gridded block letters graphic!
+
+//include library
+#include <iostream>
+
+//main program
+int main() {
+  std::cout<< "  CCC      M       M      CCC \n";
+  std::cout<< " C   C     MM     MM     C   C\n";
+  std::cout<< " C         MM     MM     C    \n";
+  std::cout<< " C         M   M   M     C    \n";
+  std::cout<< " C         M       M     C    \n";
+  std::cout<< " C   C     M       M     C   C\n";
+  std::cout<< "  CCC      M       M      CCC \n";
+}
